6e2a23965a2c7ba395a46a6771bfc93cd9e3665e
mpusio <32813834+mpusio@users.noreply.github.com>
2019-07-20 14:37:17 +0200

    Add files via upload

---
+++ target/classes/static/js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.dropdown-trigger').dropdown();
+    $('select').formSelect();
+
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
